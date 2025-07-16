bf7a09f6a5919ebc68792713bad559d0acabde37
MarufaKompass <Marufa@hellokompass.com>
2025-07-16 16:35:42 +0600

    make it responsive

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,10 +1,8 @@
-import React from "react";
 import HeroSection from "./HeroSection";
 import HealthCare from "./HealthCare";
 import Health from "./Health";
 import Impact from "./Impact";
 import Blog from "./Blog";
-import ImageGallery from "./ImageGallery";
 import HealthChecklist from "./HealthChecklist";
 import Doctors from "./Doctors";
 export default function Home() {
@@ -14,9 +12,9 @@ export default function Home() {
       <HealthChecklist></HealthChecklist>
       <Doctors></Doctors>
       <Health></Health>
-       <HealthCare></HealthCare>
-        <Impact></Impact>
-        <Blog></Blog>
+      <HealthCare></HealthCare>
+      <Impact></Impact>
+      <Blog></Blog>
     </div>
   );
 }
